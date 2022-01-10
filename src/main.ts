e5faceebd5cccffabd61922eeb3c0a0d4cbccf9a
meuko <h@mza.jp>
2022-01-11 00:02:36 +0100

    + Lint

--- src/main.ts
+++ src/main.ts
@@ -3,72 +3,79 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-		// Set up Github tokens
+    // Set up Github tokens
     const gitToken: string = core.getInput('github-token')
-		const git = github.getOctokit(gitToken)
+    const git = github.getOctokit(gitToken)
 
-		// Set up tag target.
-		const regexp: string = core.getInput('tag-target')
-		if (regexp != "staging" && regexp != "production") {
-			return core.setFailed('Ensure that tag-target is set to either staging or production.')
-		}
+    // Set up tag target.
+    const regexp: string = core.getInput('tag-target')
+    if (regexp !== 'staging' && regexp !== 'production') {
+      return core.setFailed(
+        'Ensure that tag-target is set to either staging or production.'
+      )
+    }
 
-		// Get source context
-		const { owner, repo } = github.context.repo
+    // Get source context
+    const {owner, repo} = github.context.repo
 
-		var rexp = (regexp == "staging")
-			? new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+\-rc(\.[0-9]+)?$/) 
-			: new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+$/)	
-	
-			// Get all our releases
-			var baseError = 'Something went wrong while trying to retrieve your releases.'
-			var res = await git.request('GET  /repos/{owner}/{repo}/releases', {owner, repo})
+    const rexp =
+      regexp === 'staging'
+        ? new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+-rc(\.[0-9]+)?$/)
+        : new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+$/)
 
-			// Check if our request was a success
-			if (res.status != 200) {
-				return core.setFailed(`${baseError} Got status ${res.status} but expected status 200.`)
-			}
+    // Get all our releases
+    const baseError =
+      'Something went wrong while trying to retrieve your releases.'
+    const res = await git.request('GET  /repos/{owner}/{repo}/releases', {
+      owner,
+      repo
+    })
 
-			if (res.data.length == 0) {
-				return core.setFailed(`${baseError} Response contained no data. Ensure there are releases.`)
-			}
-			
+    // Check if our request was a success
+    if (res.status !== 200) {
+      return core.setFailed(
+        `${baseError} Got status ${res.status} but expected status 200.`
+      )
+    }
 
-		// Extract all releases
-		var releaseTagNames = []
-		for (var value of res.data) {
-			releaseTagNames.push(value.tag_name)
-		}
+    if (res.data.length === 0) {
+      return core.setFailed(
+        `${baseError} Response contained no data. Ensure there are releases.`
+      )
+    }
 
-		
-		// Since our tags are ordered, simply loop and exit at first match
-		var matchTag
-		for (var value of releaseTagNames) {
-			var matchTotal = rexp.exec(value)
-			if (!matchTotal || matchTotal.length == 0) {
-				continue
-			} else {
-				matchTag = matchTotal
-				break;
-			}
-		}
+    // Extract all releases
+    const releaseTagNames = []
+    for (const value of res.data) {
+      releaseTagNames.push(value.tag_name)
+    }
 
-		// At this point we have either a match or matchTag == undefined
-		if (!matchTag) {
-			matchTag = 'v0.0.0'
-		} else {
-			matchTag = matchTag[0] // if we have a match we're dealing with an array of results
-		}
+    // Since our tags are ordered, simply loop and exit at first match
+    let matchTag
+    for (const val of releaseTagNames) {
+      const matchTotal = rexp.exec(val)
+      if (!matchTotal || matchTotal.length === 0) {
+        continue
+      } else {
+        matchTag = matchTotal
+        break
+      }
+    }
 
-		core.setOutput('TARGET_TAG', matchTag)
+    // At this point we have either a match or matchTag == undefined
+    if (!matchTag) {
+      matchTag = 'v0.0.0'
+    } else {
+      matchTag = matchTag[0] // if we have a match we're dealing with an array of results
+    }
 
-
+    core.setOutput('TARGET_TAG', matchTag)
   } catch (error) {
     if (error instanceof Error) {
-			core.setFailed(error.message);
-		} else {
-			core.setFailed('Something went wrong.')
-		}
+      core.setFailed(error.message)
+    } else {
+      core.setFailed('Something went wrong.')
+    }
   }
 }
 
