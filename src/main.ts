9ccec814eee3d2a19892f3c56f3f607d3cc59084
meuko <h@mza.jp>
2022-01-10 23:56:38 +0100

    + Add general request + Add regex pathing

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,64 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-		// Setup all variables needed for our requests to github.
+		// Set up Github tokens
     const gitToken: string = core.getInput('github-token')
-		const regexp: string = core.getInput('tag-regexp')
 		const git = github.getOctokit(gitToken)
+
+		// Set up tag target.
+		const regexp: string = core.getInput('tag-target')
+		if (regexp != "staging" && regexp != "production") {
+			return core.setFailed('Ensure that tag-target is set to either staging or production.')
+		}
+
+		// Get source context
 		const { owner, repo } = github.context.repo
 
+		var rexp = (regexp == "staging")
+			? new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+\-rc(\.[0-9]+)?$/) 
+			: new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+$/)	
 	
-		try {
-			var regex = new RegExp(regexp);
+			// Get all our releases
+			var baseError = 'Something went wrong while trying to retrieve your releases.'
+			var res = await git.request('GET  /repos/{owner}/{repo}/releases', {owner, repo})
 
-			git.request('GET  /repos/{owner}/{repo}/tags', {owner, repo})
+			// Check if our request was a success
+			if (res.status != 200) {
+				return core.setFailed(`${baseError} Got status ${res.status} but expected status 200.`)
+			}
 
-		} catch(error) {
-			var baseError = 'Something went wrong while trying to parse the regular expression.'
-			if (error instanceof Error) {
-				core.setFailed(baseError + ' Are you sure it\'s a valid one? \n'+ error.message)
+			if (res.data.length == 0) {
+				return core.setFailed(`${baseError} Response contained no data. Ensure there are releases.`)
+			}
+			
+
+		// Extract all releases
+		var releaseTagNames = []
+		for (var value of res.data) {
+			releaseTagNames.push(value.tag_name)
+		}
+
+		
+		// Since our tags are ordered, simply loop and exit at first match
+		var matchTag
+		for (var value of releaseTagNames) {
+			var matchTotal = rexp.exec(value)
+			if (!matchTotal || matchTotal.length == 0) {
+				continue
 			} else {
-				core.setFailed(baseError)
+				matchTag = matchTotal
+				break;
 			}
 		}
+
+		// At this point we have either a match or matchTag == undefined
+		if (!matchTag) {
+			matchTag = 'v0.0.0'
+		} else {
+			matchTag = matchTag[0] // if we have a match we're dealing with an array of results
+		}
+
+		core.setOutput('TARGET_TAG', matchTag)
 
 
   } catch (error) {
